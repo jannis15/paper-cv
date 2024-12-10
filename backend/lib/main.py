9e40c2186925366458f00d74111bc9dbfdb88c47
J <jannis_brake@web.de>
2024-12-10 22:55:13 +0100

    add custom limit exceeded json response

--- backend/lib/main.py
+++ backend/lib/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, UploadFile, Request
+from fastapi.responses import JSONResponse
 from lib.floor_cv_controller import FloorCvController
 from dotenv import load_dotenv
 from google.cloud import vision
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 load_dotenv()
 app = FastAPI()
@@ -23,6 +25,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_error(request, exc):
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "Rate limit exceeded"},
+    )
 
 
 @app.post('/scan')
