10a5d2d9bb122dfceed6fc2244c00d3328cc68ca
J <jannis_brake@web.de>
2024-12-10 21:05:56 +0100

    add tooltip for button

--- frontend/lib/components/floor_contact_banner.dart
+++ frontend/lib/components/floor_contact_banner.dart
@@ -77,6 +77,7 @@ class _FloorContactBannerState extends State<FloorContactBanner> {
               top: 0,
               right: 0,
               child: IconButton(
+                tooltip: 'Schließen',
                 icon: Icon(Icons.close),
                 onPressed: widget.onCloseBanner,
               ),
