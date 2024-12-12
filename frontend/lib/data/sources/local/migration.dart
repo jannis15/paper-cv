9ded87b8713d87e1928aa337bafba57d6dd50228
J <jannis_brake@web.de>
2024-12-12 03:02:10 +0100

    delete 3 sample documents

--- frontend/lib/data/sources/local/migration.dart
+++ frontend/lib/data/sources/local/migration.dart
@@ -8,38 +8,6 @@ extension Migration on FloorDatabase {
         beforeOpen: (details) async {},
         onCreate: (migrator) async {
           await migrator.createAll();
-          Future<void> insertDocument(TbDocumentCompanion entry) async {
-            await into(tbDocument).insert(entry);
-          }
-
-          Future<void> insertTestData() async {
-            final now = DateTime.now();
-            List<TbDocumentCompanion> testData = [
-              TbDocumentCompanion(
-                title: Value('Dokument 1'),
-                notes: Value(''),
-                createdAt: Value(now),
-                modifiedAt: Value(now),
-              ),
-              TbDocumentCompanion(
-                title: Value('Dokument 2'),
-                notes: Value(''),
-                createdAt: Value(now.subtract(const Duration(days: 1))),
-                modifiedAt: Value(now),
-              ),
-              TbDocumentCompanion(
-                title: Value('Dokument 3'),
-                notes: Value(''),
-                createdAt: Value(now.subtract(const Duration(days: 2))),
-                modifiedAt: Value(now.subtract(const Duration(days: 1))),
-              ),
-            ];
-            for (final doc in testData) {
-              await insertDocument(doc);
-            }
-          }
-
-          await insertTestData();
         },
         onUpgrade: (migrator, from, to) async {},
       );
