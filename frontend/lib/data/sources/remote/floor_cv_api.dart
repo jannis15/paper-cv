9b31d4d911993037153284a8aef7cef41309a125
J <jannis_brake@web.de>
2024-12-12 22:27:38 +0100

    change to prod url

--- frontend/lib/data/sources/remote/floor_cv_api.dart
+++ frontend/lib/data/sources/remote/floor_cv_api.dart
@@ -4,7 +4,7 @@ import 'package:paper_cv/utils/api_utils.dart';
 import 'package:paper_cv/utils/file_picker_models.dart';
 
 class FloorCvApi extends RestApi {
-  static final FloorCvApi _instance = FloorCvApi(baseUrl: 'http://192.168.178.46:443');
+  static final FloorCvApi _instance = FloorCvApi(baseUrl: 'https://floor-cv-670589174841.europe-west1.run.app');
 
   FloorCvApi({required super.baseUrl});
 
